94dd7675863671efdcae8517357ae4837dc64c42
Natalie <nataliekkeiger@gmail.com>
2025-10-06 18:29:28 -0400

    copied work from teammates repo

--- 17_form-flex/app.py
+++ 17_form-flex/app.py
@@ -1,7 +1,9 @@
 # Salt Lamp Horse
 # Natalie Keiger, Evan Khosh, Carrie Ko
 # SoftDev
+# K17: Take and Give
 # October 2025
+# time taken: 30m
 
 # import conventions:
 # list most general first (standard python library)
@@ -12,32 +14,11 @@ from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
 
-import testmod0                     #"home-rolled" module for basic exploration
-
 #the conventional way:
 #from flask import Flask, render_template, request
 
 app = Flask(__name__)    #create Flask object
 
-
-'''
-trioTASK:
-~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
-...read for understanding all of the code below.
- * Some will work as written;
- *  ...other sections will NOT.
-
-TASK: Predict which.
- 1. Devise simple tests to isolate components/behaviors.
- 2. Execute your tests.
- 3. Process results.
- 4. Findings yield new ideas for more tests? Yes: do them.
-
-PROTIP: Insert your own in-line comments
- wherever they will help
-  your future self and/or current teammates
-   understand what is going on.
-'''
 
 @app.route("/" , methods=['GET'])
 def disp_loginpage():
@@ -55,7 +36,7 @@ def disp_loginpage():
     return render_template( 'login.html' )
 
 
-@app.route("/auth"  , methods=['POST'])
+@app.route("/auth"  , methods=['GET'])
 def authenticate():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
@@ -65,10 +46,10 @@ def authenticate():
     print("***DIAG: request.args ***")
     print(request.args)
     print("***DIAG: request.args['username']  ***")
-    print(request.form['username'])
+    print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
-    return request.form['username']  #puts your username on the page
+    return render_template('response.html', user = request.args['username'], request = str(request)[-7:])  #puts your username on the page
 
     
 if __name__ == "__main__": #false if this file imported as module
