dcd63c3226e2f6b7018fe52276e3a434e099997b
Natalie <nataliekkeiger@gmail.com>
2026-01-07 11:43:21 -0500

    i hate racket

---
+++ 34_scm/fibfac.scm
@@ -0,0 +1,33 @@
+#lang racket
+
+; Natalie Keiger
+; cerulean
+; 01-07-2026
+; K-34
+; 10ish minutes
+
+(define (fact n)
+  (if (< n 0)
+      "Cannot use negative numbers"
+      (if (= n 0)
+          1
+           (* n (fact (- n 1))))))
+
+(define (fib n)
+    (if (< n 0)
+      "Cannot use negative numbers"
+      (if (= n 0)
+          1
+          (+ n (fib (- n 1))))))
+
+(fact 4)
+(fact 7)
+(fact 1)
+(fact 2)
+(fact -3)
+(fib 1)
+(fib 5)
+(fib 3)
+(fib 10)
+(fib -1)
+
